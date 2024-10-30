433c5be6192137ddb047b7884d6ed569907099cd
Franco Tobias Martinez <francomartinez.trabajo@gmail.com>
2024-10-30 17:44:17 -0300

    first commit

---
+++ fmfCorp-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fmfCorp-app',
+  webDir: 'www'
+};
+
+export default config;
