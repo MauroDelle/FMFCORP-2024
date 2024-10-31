489da01be6f1e7b8b0ae660b1e7feea4bf92730e
Franco Tobias Martinez <francomartinez.trabajo@gmail.com>
2024-10-31 19:16:50 -0300

    splash dinamico

--- fmfCorp-app/src/app/app.routes.ts
+++ fmfCorp-app/src/app/app.routes.ts
@@ -4,10 +4,10 @@ export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
+  },  
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'splash',
+    loadComponent: () => import('./splash/splash.component').then((m) => m.SplashComponent),
   },
+  { path: '', redirectTo: 'home', pathMatch: 'full',},
 ];
