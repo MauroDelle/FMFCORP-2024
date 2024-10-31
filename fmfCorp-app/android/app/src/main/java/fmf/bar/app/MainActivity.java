da9763f83638b434d3f9e57b1455fd9bb779340e
Franco Tobias Martinez <francomartinez.trabajo@gmail.com>
2024-10-30 22:56:25 -0300

    Creacion de logo y cambio en el appId para no mostrar nombre deafult en la app de andoid

---
+++ fmfCorp-app/android/app/src/main/java/fmf/bar/app/MainActivity.java
@@ -0,0 +1,5 @@
+package fmf.bar.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- fmfCorp-app/capacitor.config.ts
+++ fmfCorp-app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'fmf.bar.app',
   appName: 'fmfCorp-app',
   webDir: 'www'
 };
