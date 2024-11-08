f190006e84715b6bc02b68c86b638b544cdb51a6
mauro <maurodelle7@gmail.com>
2024-11-08 10:22:44 -0300

    Arreglo Template y Connection String

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBlrdSM53jysbRqtamk49KGfNOQinGoaEk",
-    authDomain: "fmfcorp-2024.firebaseapp.com",
-    projectId: "fmfcorp-2024",
-    storageBucket: "fmfcorp-2024.firebasestorage.app",
-    messagingSenderId: "840951034886",
-    appId: "1:840951034886:web:379469070cb7cd83d97f87"
+    apiKey: "AIzaSyD0D0hBDUAopqoUmPflWS4ZKU4e8fwBDAM",
+    authDomain: "relevamiento-visual1-pps.firebaseapp.com",
+    projectId: "relevamiento-visual1-pps",
+    storageBucket: "relevamiento-visual1-pps.appspot.com",
+    messagingSenderId: "879394204092",
+    appId: "1:879394204092:web:747990fe2a1d4620dce3b9"
   }
 };
