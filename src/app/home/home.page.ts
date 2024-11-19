27303f0bf8e225ca4f138e6bda2c4347aef89fd9
mauro <maurodelle7@gmail.com>
2024-11-19 19:12:00 -0300

    Agrandando Fuentes y montos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -182,7 +182,6 @@ export class HomePage implements OnInit, OnDestroy {
   }
 
   async scan(): Promise<void> {
-    this.router.navigate(['menu']);
     const { barcodes } = await BarcodeScanner.scan();
     if (barcodes.length > 0) {
       this.informacionQr = barcodes[0].rawValue;  // Asignar la información del primer código QR escaneado
